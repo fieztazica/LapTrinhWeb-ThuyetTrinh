acd60d9b15475fcf8a4acbe4c23e3044c7c3fdad
Fiezt <dat09t@gmail.com>
2023-03-06 23:30:46 +0700

    Modified

--- demo-next/next.config.js
+++ demo-next/next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     async rewrites() {
         return [
             {
                 source: '/api/:path*',
-                destination: 'https://localhost:64027/api/:path*',
+                destination: 'http://localhost:64027/api/:path*',
             },
         ]
     },
--- demo-next/pages/test.js
+++ demo-next/pages/test.js
@@ -1,35 +1,53 @@
 import { useEffect, useState } from 'react'
 
 async function getQuote() {
-    const data = await fetch(`/api/values/5`).then(res => res.json());
-    console.log(data)
+    const data = await fetch(`http://localhost:3000/api/values/5`).then(res => res.json());
+    //console.log(data)
     return data;
 }
 
-export default function Test({ data }) {
+export default function Test(props) {
 
     const [quote, setQuote] = useState(null);
+    //const [bool, setBool] = useState(false);
+    const { data } = props;
+
+    /**
+     * 
+     * setTimeout(() => {
+        getQuote().then((q) => setQuote(q))
+    }, 5000)
+     */
 
     useEffect(() => {
-        getQuote().then((q) => setQuote(q))
+        //console.log("first");
+
+        setTimeout(() => {
+            getQuote().then((q) => setQuote(q))
+        }, 5000)
+
+        //setInterval(() => {
+        //    console.log("10s")
+        //    setBool(b => !b);
+        //}, 10000)
     }, []);
 
-    console.log(data)
+    //console.log(data)
     return <div>
-        <p>
-            {data}
-        </p>
-        <p>
-            {
-                quote ? quote : "Loading..."
-            }
-        </p>
+        <h1>
+            SSR: {data}
+        </h1>
+        <h1>
+            CSR: {quote ? quote : "Loading..."}
+        </h1>
     </div>;
 }
 
 export async function getServerSideProps(context) {
-    const data = await getQuote()
+    const data = await getQuote();
     //const data = "abc"
+
+    //console.log(context);
 
     return {
         props: {
